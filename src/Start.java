918ce0de216ae997c5d360920f95f2957d9d227f
yulimm34 <yulimm34@naver.com>
2023-11-22 17:30:10 +0900

    center

--- src/Start.java
+++ src/Start.java
@@ -12,22 +12,19 @@ public class Start extends JFrame {
     private JTextField tf = new JTextField(20);
 
     public Start() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
         setTitle("TETRIS");
         setSize(500, 500);
-        setContentPane(new MyPanel());
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Container c = getContentPane();
-        c.setLayout(new FlowLayout());
+        JLabel logo = new JLabel("Tetris");
+        logo.setFont(new Font("Jokerman", Font.ITALIC, 55));
+        logo.setForeground(Color.YELLOW);
+
         JLabel id = new JLabel("ID");
-        id.setBounds(1600, 1500, 100, 80);
-        c.add(id);
-        tf.setLocation(1600, 1600);
-        c.add(tf);
-
         JButton bt = new JButton("START");
-        bt.addActionListener(new ActionListener() {
 
+        bt.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("CLICK START!!");
@@ -45,47 +42,27 @@ public class Start extends JFrame {
                     }
                 }
                 System.out.println(temp);
-
             }
         });
 
-        //bt.setBounds(1200, 500, 100, 80); //앞이 좌표 뒤가 크기
-        setLayout(new FlowLayout(FlowLayout.CENTER));
-        c.add(bt);
-        setLocationRelativeTo(null);
+        logo.setSize(500,100);
+        logo.setHorizontalAlignment(JLabel.CENTER);
+        id.setSize(100, 80);
+        tf.setSize(10, 10);
+        bt.setSize(100, 80);
+
+        JPanel panel = new JPanel(new GridBagLayout());
+        panel.add(id);
+        panel.add(tf);
+        panel.add(bt);
+        add(logo);
+        add(panel);
+
         setVisible(true);
 
     }
 
-    class MyPanel extends JPanel {
-        private ImageIcon icon = new ImageIcon("Tetris.PNG");
-        private Image img = icon.getImage();
-        private Vector<Point> sv = new Vector<Point>();
-
-
-        public MyPanel() {
-            this.addComponentListener(new ComponentAdapter() {
-                public void componentResized(ComponentEvent e) {
-
-                    MyPanel.this.removeComponentListener(this);
-                }
-            });
-        }
-
-        @Override
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-
-            g.setFont(new Font("Jokerman", Font.ITALIC, 55));
-            g.setColor(Color.YELLOW);
-            g.drawString("TETRIS", 50, 150);
-        }
-    }
-
     public static void main(String[] args) throws IOException {
-//		new ImageSource();
-//		new GameFrame();
-
         new Start();
         System.out.println("goodies");
 
